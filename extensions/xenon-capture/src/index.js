6365b74a4f48eabc6005335038a759bc2e27b314
Teon Lucas <teon.max.lucas@gmail.com>
2024-04-29 18:25:17 -0700

    Simplify to extension-only app, add Xenon.init()

--- extensions/xenon-capture/src/index.js
+++ extensions/xenon-capture/src/index.js
@@ -1,19 +1,37 @@
 import {register} from "@shopify/web-pixels-extension";
+import Xenon from 'xenon-view-sdk';
 
 register((api) => {
-    // Set a cookie example
-    api.browser.cookie.set('app_name', 'xenon-capture');
+  // See if debug is set
+  const debug = api.settings.debug && (api.settings.debug === "1" || api.settings.debug.toLowerCase() === "true");
 
-    // Subscribe to standard events
-    api.analytics.subscribe('all_standard_events', (event) => {
+  // Register Xenon
+  if (api.settings.apiKey) {
+    Xenon.init(api.settings.apiKey);
+    const xenonId = Xenon.id()
+    api.browser.localStorage.setItem('xenonId', xenonId).then(() => {
+      if (debug) {
+        console.log('xenon-id:', xenonId);
+      }
+    });
+  } else {
+    console.log('Error: No Xenon API Key configured');
+  }
+
+  // Subscribe to standard events
+  api.analytics.subscribe('all_standard_events', (event) => {
+    if (debug) {
       console.log('xenon-capture event:', event);
       console.log('xenon-capture init:', api.init);
-    });
+    }
+  });
 
-    // Subscribe to xenon events (can also subscribe to all_custom_events)
-    api.analytics.subscribe('xenon', (event) => {
-      const timing = JSON.parse(event.customData.timing);
+  // Subscribe to xenon events (can also subscribe to all_custom_events)
+  api.analytics.subscribe('xenon', (event) => {
+    const timing = JSON.parse(event.customData.timing);
+    if (debug) {
       console.log('xenon-capture timing:', timing);
       console.log('xenon-capture settings:', api.settings);
-    });
+    }
+  });
 });
